f8700f6fc99aee4b24fab27f5731e049a19858cf
Nicolas Paz <nicopazmalizia@gmail.com>
2026-01-10 02:37:45 -0300

    Fix Preloader types final

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -1,16 +1,16 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion"; // <--- 1. Importamos 'Variants'
 
 
 export default function Preloader() {
-  const slideUp = {
+  
+  const slideUp: Variants = {
     initial: { y: 0 },
     exit: { 
       y: "-100vh", 
       transition: { 
         duration: 0.8, 
-        
-        ease: [0.76, 0, 0.24, 1] as [number, number, number, number] 
+        ease: [0.76, 0, 0.24, 1] as const 
       } 
     }
   };
@@ -22,14 +22,11 @@ export default function Preloader() {
         exit="exit" 
         className="fixed inset-0 z-[999] flex items-center justify-center bg-criv-yellow"
     >
-        {/* ENVOLTORIO FLEX: Para poner Logo e Imagen lado a lado */}
+        
         <div className="relative flex items-center gap-4 md:gap-8 overflow-hidden px-4">
             
-         
-
-            {/* 2. EL TEXTO */}
             <h1 className="text-black text-6xl md:text-9xl font-display font-black tracking-tighter uppercase leading-none">
-                CRIV<span className="text-black">.</span>
+                CRIV.
             </h1>
 
         </div>
